13c2356708e477c067476b9e68110f9a3aa5d7e0
Peter Gagliardi <ptrgags@live.com>
2015-12-13 10:08:32 -0500

    Add several more sensors
    Add a bunch of new sensors to the app. Not every one works on my phone.

--- SensorLab/app/src/main/java/ptrgags/sensorlab/MainActivity.java
+++ SensorLab/app/src/main/java/ptrgags/sensorlab/MainActivity.java
@@ -12,6 +12,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private SensorManager sensorManager;
     private Sensor gravity;
+    private Sensor ambientTemp;
+    private Sensor steps;
+    private Sensor proximity;
+    private Sensor ambientLight;
+    private Sensor ambientPressure;
+    private Sensor ambientRelHumidity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +27,23 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         //Create the sensor manager
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
-        //Get the accelerometer
+        //Motion Sensors
+        //acceleration = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         gravity = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
-        sensorManager.registerListener(this, gravity, SensorManager.SENSOR_DELAY_FASTEST);
+        //gyro = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        steps = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
+        //Position sensors
+        //magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        proximity = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        //Ambient environment sensors
+        ambientTemp = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        ambientLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        ambientPressure = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        ambientRelHumidity = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+
+        registerListeners();
     }
 
     @Override
@@ -40,7 +60,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     protected void onResume() {
-        sensorManager.registerListener(this, gravity, SensorManager.SENSOR_DELAY_FASTEST);
+        registerListeners();
         super.onResume();
     }
 
@@ -58,10 +78,39 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             ((TextView) findViewById(R.id.gravityY)).setText(getString(R.string.gravityY, gravity[1]));
             ((TextView) findViewById(R.id.gravityZ)).setText(getString(R.string.gravityZ, gravity[2]));
         }
+        else if (event.sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE) {
+            ((TextView) findViewById(R.id.ambientTemp)).setText(getString(R.string.ambientTemp, event.values[0]));
+        }
+        else if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+            ((TextView) findViewById(R.id.ambientLight)).setText(getString(R.string.ambientLight, event.values[0]));
+        }
+        else if (event.sensor.getType() == Sensor.TYPE_PRESSURE) {
+            ((TextView) findViewById(R.id.ambientPressure)).setText(getString(R.string.ambientPressure, event.values[0]));
+        }
+        else if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+            ((TextView) findViewById(R.id.proximity)).setText(getString(R.string.proximity, event.values[0]));
+        }
+        else if (event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            ((TextView) findViewById(R.id.steps)).setText(getString(R.string.steps, event.values[0]));
+        }
+        else if (event.sensor.getType() == Sensor.TYPE_RELATIVE_HUMIDITY) {
+            ((TextView) findViewById(R.id.ambientRelHumidity)).setText(getString(R.string.ambientRelHumidity, event.values[0]));
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         //Do nothing
+    }
+
+
+    public void registerListeners() {
+        sensorManager.registerListener(this, gravity, SensorManager.SENSOR_DELAY_FASTEST);
+        sensorManager.registerListener(this, ambientTemp, SensorManager.SENSOR_DELAY_FASTEST);
+        sensorManager.registerListener(this, ambientLight, SensorManager.SENSOR_DELAY_FASTEST);
+        sensorManager.registerListener(this, ambientPressure, SensorManager.SENSOR_DELAY_FASTEST);
+        sensorManager.registerListener(this, ambientRelHumidity, SensorManager.SENSOR_DELAY_FASTEST);
+        sensorManager.registerListener(this, steps, SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(this, proximity, SensorManager.SENSOR_DELAY_FASTEST);
     }
 }
